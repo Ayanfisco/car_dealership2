921352ab6771c0f617413e8397bf7ecf2a142732
Ayanfisco <Ayanfiscoss@gmail.com>
2025-07-24 15:05:58 +0100

    Sale

--- addons/car_dealership/models/fleet_vehicle.py
+++ addons/car_dealership/models/fleet_vehicle.py
@@ -1,4 +1,4 @@
-# Extend fleet.vehicle to create a dealership record when applicable
+# models/fleet_vehicle.py
 from odoo import models, fields, api, _
 
 class FleetVehicle(models.Model):
@@ -9,7 +9,17 @@ class FleetVehicle(models.Model):
     sale_id = fields.Many2one('sale.order', string='Sale Order', help='Sale order associated with this vehicle')
     sale_status = fields.Selection(related='sale_id.state', string='Sale Status', store=True)
 
-    @api.onchange('fleet_status')
-    def _onchange_fleet_status(self):
-        if self.sale_status == 'sale':
-            self.state_id = 'sold'
+    @api.onchange('sale_status')
+    def _onchange_sale_status(self):
+        """Update fleet vehicle state to 'sold' when sale order is confirmed"""
+        if self.sale_status == 'sale':  # Sale order confirmed
+            # Find the 'sold' state record
+            sold_state = self.env['fleet.vehicle.state'].search([('name', '=', 'Sold')], limit=1)
+            if sold_state:
+                self.state_id = sold_state.id
+
+    # def mark_as_sold(self):
+    #     """Manual method to mark vehicle as sold"""
+    #     sold_state = self.env['fleet.vehicle.state'].search([('name', '=', 'Sold')], limit=1)
+    #     if sold_state:
+    #         self.state_id = sold_state.id

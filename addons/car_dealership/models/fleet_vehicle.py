9ed4dc7f005fc1d004602cfc48fd58fe7d3f744f
Ayanfisco <Ayanfiscoss@gmail.com>
2025-07-24 14:52:08 +0100

    Availability commit

--- addons/car_dealership/models/fleet_vehicle.py
+++ addons/car_dealership/models/fleet_vehicle.py
@@ -6,10 +6,10 @@ class FleetVehicle(models.Model):
 
     product_id = fields.Many2one('product.template', string='Dealership Vehicle')
     fleet_status = fields.Selection(related='product_id.state', string='Fleet Status', store=True)
+    sale_id = fields.Many2one('sale.order', string='Sale Order', help='Sale order associated with this vehicle')
+    sale_status = fields.Selection(related='sale_id.state', string='Sale Status', store=True)
 
     @api.onchange('fleet_status')
     def _onchange_fleet_status(self):
-        if self.fleet_status == 'available':
-            self.state_id = 'available'
-        elif self.fleet_status == 'sold':
+        if self.sale_status == 'sale':
             self.state_id = 'sold'

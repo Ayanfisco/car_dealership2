75d4c9f726182bc59feca4451bdb71d0eba36116
Ayanfisco <Ayanfiscoss@gmail.com>
2025-07-24 15:47:39 +0100

    Complete

--- addons/car_dealership/models/fleet_vehicle.py
+++ addons/car_dealership/models/fleet_vehicle.py
@@ -1,24 +1,17 @@
+# fleet_vehicle.py
 from odoo import models, fields, api, _
+from odoo.exceptions import ValidationError
 
 class FleetVehicle(models.Model):
     _inherit = 'fleet.vehicle'
 
-    product_id = fields.Many2one('product.template', string='Dealership Vehicle')
-    fleet_status = fields.Selection(related='product_id.state', string='Fleet Status', store=True)
-    sale_id = fields.Many2one('sale.order', string='Sale Order', help='Sale order associated with this vehicle')
-    sale_status = fields.Selection(related='sale_id.state', string='Sale Status', store=True)
-    sold_state_id = fields.Many2one('fleet.vehicle.state', compute='_compute_state_sold', store=True)
-
-    @api.depends('sale_id.state')
-    def _compute_state_sold(self):
-        sold_state = self.env['fleet.vehicle.state'].search([('name', '=', 'Sold')], limit=1)
-        for rec in self:
-            if rec.sale_id and rec.sale_id.state == 'sale' and sold_state:
-                rec.state_id = sold_state.id
-
-
-    # def mark_as_sold(self):
-    #     """Manual method to mark vehicle as sold"""
-    #     sold_state = self.env['fleet.vehicle.state'].search([('name', '=', 'Sold')], limit=1)
-    #     if sold_state:
-    #         self.state_id = sold_state.id
+    def mark_vehicle_as_sold_by_lot(self, lot_name, sale_order):
+        """Find vehicle by license_plate and mark as Sold"""
+        vehicle = self.search([('license_plate', '=', lot_name)], limit=1)
+        if vehicle:
+            sold_state = self.env['fleet.vehicle.state'].search([('name', '=', 'Sold')], limit=1)
+            if sold_state:
+                vehicle.write({
+                    'state_id': sold_state.id,
+                    'sale_id': sale_order.id,
+                })
--- addons/car_dealership/models/sale_order.py
+++ addons/car_dealership/models/sale_order.py
@@ -8,3 +8,13 @@ class SaleOrder(models.Model):
 
 
     is_vehicle = fields.Boolean(related='vehicle_id.is_vehicle', string='is_vehicle', store=True)
+
+    def action_confirm(self):
+        res = super().action_confirm()
+
+        for order in self:
+            for line in order.order_line:
+                for lot in line.lot_ids:
+                    self.env['fleet.vehicle'].mark_vehicle_as_sold_by_lot(lot.name, order)
+
+        return res

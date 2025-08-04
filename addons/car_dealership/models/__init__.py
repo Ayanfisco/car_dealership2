060c76fa843a19d210e0de79317e03c53b41e2db
Ayanfisco <Ayanfiscoss@gmail.com>
2025-08-04 13:33:44 +0100

    GP

--- addons/car_dealership/models/__init__.py
+++ addons/car_dealership/models/__init__.py
@@ -1,6 +1,6 @@
 from . import dealership_vehicle
 from . import product_template
-from . import fleet_vehicle
+# from . import fleet_vehicle
 from . import purchase_order
 from . import fleet_vehicle_state
 from . import stock_move_line
--- addons/car_dealership/models/fleet_vehicle_state.py
+++ addons/car_dealership/models/fleet_vehicle_state.py
@@ -1,8 +1,9 @@
 from odoo import models, fields, api
 
+
 class FleetVehicleState(models.Model):
     _inherit = 'fleet.vehicle.state'
-    
+
     @api.model
     def _get_state_selections(self):
         # Get base selections first
@@ -11,7 +12,6 @@ class FleetVehicleState(models.Model):
         selections.append(('sold', 'Sold'))
         return selections
 
-    
     def _valid_field_parameter(self, field, name):
         # Add 'tracking' to valid parameters
         return name == 'tracking' or super()._valid_field_parameter(field, name)

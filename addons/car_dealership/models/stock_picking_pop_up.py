6bfa8e1fa92f7055e98f7f125383d9b74fdf4a9f
Ayanfisco <Ayanfiscoss@gmail.com>
2025-08-13 08:48:25 +0100

    Completed for Functional feedback

--- addons/car_dealership/models/stock_picking_pop_up.py
+++ addons/car_dealership/models/stock_picking_pop_up.py
@@ -3,20 +3,24 @@ from odoo.exceptions import UserError
 
 
 class StockPickingPopUp(models.Model):
-    """Inherits stock.picking to enforce VIN/Chassis number entry
-    for tracked products before validation.
-    """
     _inherit = 'stock.picking'
-    _description = 'Stock Picking'
 
     def button_validate(self):
         """Override the default validation to ensure a VIN/Chassis
         number is provided for tracked products.
         """
-        for move in self.move_ids_without_package:
+        for move in self.move_ids:
             product = move.product_id
-            if product.tracking != 'none' and not move.move_line_ids:
-                raise UserError(
-                    _("You need to supply a VIN/Chasis Number for product:\n- %s") % product.display_name
-                )
+            if product.tracking in ['serial', 'lot']:
+                # Check if there are move lines with tracking info
+                if not move.move_line_ids:
+                    raise UserError(
+                        _("You need to supply a VIN/Chassis Number for product:\n- %s") % product.display_name
+                    )
+                # Check each move line has lot/serial number (check both lot_id AND lot_name)
+                for move_line in move.move_line_ids:
+                    if not move_line.lot_id and not move_line.lot_name:
+                        raise UserError(
+                            _("You need to supply a VIN/Chassis Number for product:\n- %s") % product.display_name
+                        )
         return super().button_validate()
